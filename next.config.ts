4acc3bee3dcd3ffd117cc58dc6985c5f277decf4
Your Name <your-email@example.com>
2025-07-10 06:56:13 +0530

    congig modified

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.unsplash.com'],
-  },
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  }
 };
-
-module.exports = nextConfig;
